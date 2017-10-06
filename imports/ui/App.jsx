db0549ecdc151a2be88136d58eec5e8a7246f293
Andrea Paredes <aj.paredes10@uniandes.edu.co>
2017-10-06 16:54:33 -0500

    primer commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+ 
+class App extends Component {
+
+    render(){
+        return(
+            <div className="App">
+                <h1>Multiplayer Snake Game</h1>
+            </div>
+        );
+    }
+}
+App.propTypes = {
+
+};
+export default App;
